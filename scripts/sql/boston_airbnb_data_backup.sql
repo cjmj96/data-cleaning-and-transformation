9a1bf6e3861a0ff8225ee61d030eb65b44b0c2ef
Christian Javier Montenegro Jarquín <cjmj19@protonmail.ch>
2024-06-21 13:17:27 -0600

    Add final dump file

--- scripts/sql/boston_airbnb_data_backup.sql
+++ scripts/sql/boston_airbnb_data_backup.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee046eae997a2496a6cefdcbefc882bb5d7337f389e7535772334f0ee2fdfd77
-size 135133620
+oid sha256:5f0c49f74f0a411ffb7e3f134970f26eac5aeb6d795290387d898086d0fb98fb
+size 259880047
